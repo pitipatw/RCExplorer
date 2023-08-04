4598dfdb855d157d7aae0e0fb44078e4ca3d4253
Hazel <haymann@mit.edu>
2023-08-04 14:51:03 -0400

    Cleaner version
    took out the reflection function, allow the user to orient the full pixel in the range [0-360]

--- PixelGeometry.jl
+++ PixelGeometry.jl
@@ -2,6 +2,7 @@ module PixelGeometry
 using LinearAlgebra
 using GeometryBasics
 using Makie, GLMakie
+
 """
     makepixel(L::Real, t::Real, Lc::Real; n = 10)
 L = length of pixel arm
@@ -36,91 +37,46 @@ function makepixel(L::Real, t::Real, Lc::Real; n=10)
     return points
 end
 
-
-function main()
-    points = makepixel(150, 10, 10)
-    # Change points from a vector of array to an array
+"""
+rotate a set of points by desired degrees 
+"""
+function rotate_the_points(points::Vector, Degrees::Float64)
+    # change vector(vector(Float64)) into a matrix of float 64 
+    # to perform matrix operation
     point_matrix = Matrix{Float64}(undef, size(points)[1], 2)
     for row in 1:size(point_matrix)[1]
         point_matrix[row, :] = points[row]
     end
-    # change all the points to opposite sign
-    negative_points = -1 * (point_matrix)
-
-    # take original numbers for y and opposite sign numbers for x to get a reflection 
-    new_points = Array{Array{Float64,1},1}()
-    for i in 1:size(point_matrix)[1]
-        x = negative_points[i, 1]
-        y = point_matrix[i, 2]
-        push!(new_points, [x, y])
-    end
-
-
-    ## get a top pixel piece (rotation to top)
-    # using rotation matrix
-    θ = 120.0
-    # rotation_matrix = [[cosd(θ), (-1*sind(θ))] [sind(θ) ,cosd(θ)]]
-    rotation_matrix = [cosd(θ) -sind(θ) ; sind(θ) cosd(θ)]
-    new_points3 = Array{Array{Float64,1},1}()
-    # println(point_matrix)
-    # println(point_matrix[3, :])
-    # example_rotation = rotation_matrix .* point_matrix[3, :]
-    # # println(example_rotation)
-    #     y= sum(example_rotation[1])
-    #     x= sum(example_rotation[2])
-    #     println(x)
-    #     println(y)
+    rotation_matrix = [cosd(Degrees) -sind(Degrees); sind(Degrees) cosd(Degrees)]
+    NewPoints = Array{Array{Float64,1},1}()
+    # rotate each point in the matrix and store them as new points 
     for row in 1:size(point_matrix)[1]
         rotated_points = rotation_matrix * point_matrix[row, :]
         x = rotated_points[1]
         y = rotated_points[2]
-        push!(new_points3, [x, y])
+        push!(NewPoints, [x, y])
     end
-    println(new_points3)
+    return NewPoints
+end
 
-
-    #getting the top piece(rotation to top) from Keith & Pitipat's script
-
-    #extract x and y from points
-    ptx1 = [i[1] for i in points]
-    pty1 = [i[2] for i in points]
-    #remove first point (0.0)
-    ptx1 = ptx1[2:end]
-    pty1 = pty1[2:end]
-    # ptx = vcat(ptx1, -ptx1)
-    # pty = vcat(pty1, pty1)
-
-    ptx = ptx1
-    pty = pty1
-    nodes = [ptx pty]
-    #rotate to the top
-    new_points2 = Array{Array{Float64,1},1}()
-    # draw a full pixelframe section
-    for i = 1:size(nodes)[1]
-        x = nodes[i, 1]
-        y = nodes[i, 2]
-        r = sqrt(x^2 + y^2)
-        θ = atand(y / x)
-        newθ = θ + 120.0
-        newx = r * cosd(newθ)
-        newy = r * sind(newθ)
-        push!(new_points2, [newx, newy])
-    end
-    println(new_points2)
-
-    # f2 = Figure(resolution=(600, 600))
-    # ax2 = Axis(f2[1, 1], aspect=DataAspect(), xlabel="x", ylabel="y")
-    # poly!(Point2.(new_points2))
-    # return(f2)
-
+"""
+Output a full pixel with desired orientation degree
+"""
+function execute(rotation_degree::Float64)
+    extracted_points = makepixel(150, 10, 10)
+    # create a full pixel with desired degree of orientation
+    points = rotate_the_points(extracted_points, rotation_degree)
+    new_points = rotate_the_points(extracted_points, rotation_degree + 120.0)
+    new_points1 = rotate_the_points(extracted_points, rotation_degree + 240.0)
+    # plot the full pixel
     f1 = Figure(resolution=(600, 600))
     ax1 = Axis(f1[1, 1], aspect=DataAspect(), xlabel="x", ylabel="y")
     poly!(Point2.(points))
     poly!(Point2.(new_points))
-    # println(new_points2)
-    poly!(Point2.(new_points3))
+    poly!(Point2.(new_points1))
     return f1
 end
+
 end
 
-PixelGeometry.main()
+PixelGeometry.execute(45.0)

a9f03902eaafd92b0088745f365861c26ef492a8
pitipatw <pitipatw@mit.edu>
2023-08-21 17:15:39 -0400

    Connection with tree done

--- PostTensioned/main.jl
+++ PostTensioned/main.jl
@@ -14,7 +14,8 @@ include("getterrain.jl")
 cin format
 fc', as, ec, fpe, pu, mu, vu, embodied
 """
-cin = Matrix(CSV.read("results//output_$date.csv", DataFrame))
+
+# @time cin = Matrix(CSV.read("results//output_$date.csv", DataFrame))
 #HTTP connection
 function main(cin)
     #initialize the server
@@ -51,6 +52,26 @@ function main(cin)
                     ec_max = data[i]["ec_max"]
                     
                     # @show repeat([pu, mu, vu], outer = (1,nc))'
+                    if data[i]["t"] == "Beam"
+                        #calculate section with 3 pieces
+                        #have to add 
+                        np = 3
+                        #load csv with np suffix
+
+                        cin = Matrix(CSV.read("results//output_$date.csv", DataFrame))
+                    elseif data[i]["t"] == "Column"
+                        #calculate section with 4 or 2 pieces
+                        np = 4
+                        #load csv with np suffix
+                        # cin = Matrix(CSV.read("results//output_$date.csv", DataFrame))
+
+                        np = 2
+                        #load csv with np suffix
+                        # cin = Matrix(CSV.read("results//output_$date.csv", DataFrame))
+
+
+                        cin = Matrix(CSV.read("results//output_$date.csv", DataFrame))
+                    end
 
                     #I found that this might be slower than looping... here : https://julialang.org/blog/2013/09/fast-numeric/
                     global c1 = cin[:,5:7] .> repeat([pu, mu, vu], outer = (1,nc))'
@@ -85,12 +106,23 @@ function main(cin)
                                         "pu" => outr[i][j,5], 
                                         "mu" => outr[i][j,6], 
                                         "vu" => outr[i][j,7], 
-                                        "embodied" => outr[i][j,8])
+                                        "embodied" => outr[i][j,8],
+                                        "element" => data[i]["e_idx"],
+
+                                        )
                     end
                     outvod[i] = temp
                 end
 
+                #add the last spot, for optimum choice for the section
+                #What is optimum? 
+                # Minimum embodied carbon.
+                # Same ec as the previous one
+                # Same fc' as the previous one.
+                
 
+
+                global outvod
                 jsonfile = JSON.json(outvod)
                 HTTP.send(ws, jsonfile)
                 open(joinpath(@__DIR__,"output_"*filename), "w") do f

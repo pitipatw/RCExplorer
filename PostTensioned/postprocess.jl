e3653046f41d179e9264b49ad82316de892920c9
pitipatw <pitipatw@mit.edu>
2023-08-30 15:50:19 -0400

    Post process done

--- PostTensioned/postprocess.jl
+++ PostTensioned/postprocess.jl
@@ -4,12 +4,17 @@ Post processing the choices.
 # function postprocess(outvod, data)
 
 #preparing the data
-#check all of the elements
+#sort outvod by embodied carbon-> 
+
+
+
+
+#element index container
 elements_all = Vector{Int64}(undef, size(data,1))
+#element index to section index
 elements_to_sections = Dict{Int64, Vector{Int64}}()
-decisions = Vector{Int64}(undef, size(data,1))
 for i in eachindex(data)
-    elements_all[i] = parse(Float64,data[i]["e_idx"])
+    elements_all[i] = data[i]["e_idx"]
     if haskey(elements_to_sections, elements_all[i])
         elements_to_sections[elements_all[i]] = vcat(elements_to_sections[elements_all[i]], i)
     else
@@ -19,11 +24,22 @@ end
 
 elements = collect(keys(elements_to_sections))
 
+choices_sorted = Vector{DataFrame}(undef, size(data,1))
+for i in eachindex(choices_sorted)
+    temp = DataFrame(outvod[i])
+    temp[!,"order"] = 1:size(temp,1)
+    choices_sorted[i] = sort(temp, ["embodied", "ec", "as", "fpe","fc"]) 
+end
+
+# decision vector, index based on the available choices of each section.
+decisions = ones(size(data,1))
 #loop each element
 for i in eachindex(elements)
-    element_idx = elements[i] #element index (in case elements is not in order)
-    sections = elements_to_sections[element_idx] # all section indices that associate with the element
-    println("Element :$element_idx")
+    #element index (in case elements is not in order)
+    element_idx = elements[i] 
+    #all section indices that associate with the element
+    sections = elements_to_sections[element_idx] 
+    println("Element: $element_idx")
     println("   Sections: $sections")
     # number of sections in that element
     ns = size(sections,1)
@@ -41,34 +57,39 @@ for i in eachindex(elements)
         mid_section_idx = (ns+1)/2 + ns_min -1
     end
     mid_section_idx = Int(mid_section_idx)
+    println("Mid section of e: $element_idx is $mid_section_idx")
 
-    #choices for mid section
-    mid_section_choices = DataFrame(outvod[mid_section_idx])
-    println("There are $(size(mid_section_choices,1)) choices for mid section $mid_section_idx")
-    #get order label
-    mid_section_choices[!,"order"] = 1:size(mid_section_choices,1)
-    #will loop based on this order
-    mid_section_choices_sorted = sort(mid_section_choices , ["embodied", "fpe", "as", "fc","ec"])
-    
+    #choices for mid section [a DataFrame]
+    mid_section_choices_sorted = choices_sorted[mid_section_idx]
+    println("There are $(size(mid_section_choices_sorted,1)) choices for mid section $mid_section_idx")
+
+    #current choice for mid section
     pick_idx_mid = 1
-    pick_idx = 1 
 
+    #initiate section index
     current_section_idx = ns_min
+    #if prod(element_decisions) == 1 (true), then all sections are resolved.
     element_decisions = Bool.(zeros(ns,1))
     # element_selections = Vector{Int64}(undef, ns)
     counter = 0
-    while !prod(element_decisions) && counter < 10000
+    while !prod(element_decisions) 
         counter += 1
-        println("Mid idx: $mid_section_idx")
-        println("Current section index: $current_section_idx")
-        if current_section_idx > ns_max #loop entire section of this element, but still doesnt find all decisions
-            #reset with pick_idx_mid +=1 
+        if counter >= 100000
+            println("No solution found within current counter\n consider increasing the counter limit") 
+            break
+        end
+
+        if current_section_idx > ns_max 
+            #reach the last section, and still can't find solution
+            #Use next mid section choice.
             pick_idx_mid +=1
+            #reset section index.
             current_section_idx = ns_min
             
-        elseif pick_idx_mid > size(mid_section_choices,1)
+            if pick_idx_mid > size(mid_section_choices_sorted,1)
                 println("Unresolved sections for elements $element_idx")
                 break
+            end
     
         elseif current_section_idx != mid_section_idx #skip mid section
             pick_mid_choice = mid_section_choices_sorted[pick_idx_mid,:]
@@ -76,9 +97,9 @@ for i in eachindex(elements)
             pick_mid_as = pick_mid_choice["as"]
             pick_mid_fpe = pick_mid_choice["fpe"]
             pick_idx_mid_abs = pick_mid_choice["order"]
-
+            decisions[mid_section_idx] = pick_idx_mid_abs
             #find as and fpe in the current section 
-            current_section_choices = DataFrame(outvod[current_section_idx])
+            current_section_choices = choices_sorted[current_section_idx]
 
             #only get the choices that has as and fpe
             feasible_choices = current_section_choices[(current_section_choices[:,"as"] .== pick_mid_as) .& (current_section_choices[:,"fpe"] .== pick_mid_fpe),:]
@@ -89,15 +110,15 @@ for i in eachindex(elements)
                 #and start all over again.
                 current_section_idx = ns_min
                 pick_idx_mid +=1
+                
                 continue
             else
                 #if there are available choices 
-                @show decisions[current_section_idx] = pick_idx
+                decisions[current_section_idx] = feasible_choices[1, "order"]
                 # element_selections[current_section_idx - ns_min + 1] = pick_idx
-                @show element_decisions[current_section_idx - ns_min + 1] = true
+                element_decisions[current_section_idx - ns_min + 1] = true
                 #move on to the next section in this element
                 current_section_idx += 1
-                pick_idx = 1 #reset for this index
             end
         else
             #current section is mid section
@@ -110,110 +131,78 @@ end
 # return decisions
 # end
 
-
-
-        #get choices for the current section
-        global current_section_choices = DatFrame(outvod[current_section_idx])
-        section_choices[!,"order"] = 1:size(section_choices,1)
-
-        section_choices_sorted = sort(section_choices , ["embodied", "fpe"])
-        abs_idx = sorted_choices[pick_idx,"order"] 
-        pick_idx = 1 
-    
-        while pick_idx <= length(section_choices)
-
-
-            
-
-            pick_idx += 1
-
-
-            #see if as and fpe works for the others. 
-            #if not, then move on to the next pick
-            #if yes, then keep it
-
-
-
-
-    #now, we go through other section.
-
-    idx += 1 
-
-    while idx <= ns_max
-        choices = DatFrame(outvod[idx])
-        choices[!,"order"] = 1:size(choices,1)
-
-        possible_choices = choices[(choices[:,"as"] .== pick_as) .& (choices[:,"fpe"] .== pick_fpe),:]
-        #see if as and fpe works for the others. 
-        #if not, then move on to the next pick
-        #if yes, then keep it
-
-        #pick the first one that works
-        #if none works,
-
-        println(possible_choics)
+#visualize decisions vector
+#go through each decision, get
+# fc', as, fpe, ec, embodied.
+traces = Vector{Any}(undef, size(data,1))
+set_of_results = []
+for i in eachindex(elements)
+    element = elements[i]
+    sections = elements_to_sections[element]
+    results = Matrix{Float64}(undef, size(sections,1), 5)
+    for j in eachindex(sections) 
+        section = sections[j]
+        decision_idx = Int(decisions[section])
+        decision = choices_sorted[section][decision_idx,:]
+        results[j,:] = [decision["fc"], decision["as"], decision["fpe"], decision["ec"], decision["embodied"]]
     end
+    push!(set_of_results, results)
+end
 
 
-    global nc = size(choices,1) #number of choices
-    global set_fc′ =Vector{Float64}(undef, nc)
-    global set_as  = Vector{Float64}(undef, nc)
-    global set_ec  = Vector{Float64}(undef, nc)
-    global set_fpe = Vector{Float64}(undef, nc)
-    global set_pu  = Vector{Float64}(undef, nc)
-    global set_mu  = Vector{Float64}(undef, nc)
-    global set_vu  = Vector{Float64}(undef, nc)
-    global set_embodied = Vector{Float64}(undef, nc)
-    global set_element  = Vector{Int64}(undef, nc)
+#plot the result
+traces = Vector{GenericTrace{Dict{Symbol, Any}}}()
+L = 192
+#have to slice results into elements
+for i in eachindex(elements) 
+    element = elements[i]
+    sections = elements_to_sections[element]
+    xpos = 50.0*(0:size(sections,1))
+    trace1 = scatter(; x= xpos, y = -set_of_results[i][:,4].*L, mode = "lines+markers", name = "ec")
+    push!(traces, trace1)
+end
+layout = Layout(;title="Eccentricity plot",
+yaxis_range=[-1.3,0].*L, legend_y=0.5, legend_yref="paper",
+legend=attr(family="Arial, sans-serif", size=20,
+            color="grey"))
+p1 = plot(traces, layout)
 
-    for j in eachindex(choices) 
-        set_fc′[j] = choices[j]["fc"]
-        set_as[j] = choices[j]["as"]
-        set_ec[j] = choices[j]["ec"]
-        set_fpe[j] = choices[j]["fpe"]
-        set_pu[j] = choices[j]["pu"]
-        set_mu[j] = choices[j]["mu"]
-        set_vu[j] = choices[j]["vu"]
-        set_embodied[j] = choices[j]["embodied"]
-        set_element[j] = parse(Int,choices[j]["element"])
-        @assert set_element[j] == element_idx
-    end
+traces = Vector{GenericTrace{Dict{Symbol, Any}}}()
 
-    min_embodied = minimum(set_embodied)
-    idx_min_embodied = findall(x->x==min_embodied, set_embodied)
-    for k in eachindex(idx_min_embodied) 
-        fc' = set_fc′[idx_min_embodied[k]]
-        as = set_as[idx_min_embodied[k]]
-        ec = set_ec[idx_min_embodied[k]]
-        fpe = set_fpe[idx_min_embodied[k]]
-        pu = set_pu[idx_min_embodied[k]]
-        mu = set_mu[idx_min_embodied[k]]
-        vu = set_vu[idx_min_embodied[k]]
-        embodied = set_embodied[idx_min_embodied[k]]
-        element = set_element[idx_min_embodied[k]]
-
-    end
+#have to slice results into elements
+for i in eachindex(elements) 
+    element = elements[i]
+    sections = elements_to_sections[element]
+    xpos = 50.0*(0:size(sections,1))
+    trace1 = scatter(; x= xpos, y = set_of_results[i][:,3], mode = "lines+markers", name = "ec")
+    push!(traces, trace1)
+end
+layout = Layout(;title="fpe plot", legend_y=0.5, legend_yref="paper",
+legend=attr(family="Arial, sans-serif", size=20,
+            color="grey"))
+p2 = plot(traces, layout)
 
 
-
-
-
-
-
-    
-
-
-        #select constant fpe, fc' as
-
-
-        for si in eachindex()
+traces = Vector{GenericTrace{Dict{Symbol, Any}}}()
+#have to slice results into elements
+for i in eachindex(elements) 
+    element = elements[i]
+    sections = elements_to_sections[element]
+    xpos = 50.0*(0:size(sections,1))
+    trace1 = scatter(; x= xpos, y = set_of_results[i][:,2], mode = "lines+markers", name = "ec")
+    push!(traces, trace1)
+end
+layout = Layout(;title="as plot", legend_y=0.5, legend_yref="paper",
+legend=attr(family="Arial, sans-serif", size=20,
+            color="grey"))
+p3 = plot(traces, layout)
 
 
 
-        end
-        #reset
-        element_decisions = Bool.(zeros(ns,1))
-    end
+p = [p1  p2 p3]
+relayout!(p, title_text = "Optimum result")
+
+
 
 
 

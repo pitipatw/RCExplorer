f2b54ec7de92167267400b0c588c95d4bbc648fa
Pitipat-PC <pitipatw@mit.edu>
2023-10-17 23:37:44 -0400

    re define packages

---
+++ packagelegacy.jl
@@ -0,0 +1,22 @@
+[acfe5cae] AsapSections v0.1.0 `https://github.com/keithjlee/AsapSections#main`
+[336ed68f] CSV v0.10.11
+[a93c6f00] DataFrames v1.6.1
+[31c24e10] Distributions v0.25.102
+[e9467ef8] GLMakie v0.8.11
+[60bf3e95] GLPK v1.1.3
+[5c1252a2] GeometryBasics v0.4.9
+[3587e190] InverseFunctions v0.1.12
+[682c06a0] JSON v0.21.4
+[4076af6c] JuMP v1.15.1
+[a5e1c1ea] LatinHypercubeSampling v1.9.0
+[ee78f7c6] Makie v0.19.11
+[7f7a1694] Optimization v3.19.1
+[43a3c2be] PairPlots v1.1.1
+[f0f68f2c] PlotlyJS v0.18.11
+[f2b01f46] Roots v2.0.20
+⌅ [2913bbd2] StatsBase v0.33.21
+[0c5d862f] Symbolics v5.10.0
+[3a884ed6] UnPack v1.0.2
+[713ab87d] kjlMakie v0.1.0 `https://github.com/keithjlee/kjlMakie#master`
+[37e2e46d] LinearAlgebra
+[9a3f8284] Random

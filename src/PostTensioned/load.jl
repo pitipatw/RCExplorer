8fbe3cba6ec4f7160cfc78e19a97b7da1d90a68c
pitipat_mac <pitipatw@mit.edu>
2023-10-23 04:10:40 -0400

    Plot spans x bays to see space of Mu

--- src/PostTensioned/load.jl
+++ src/PostTensioned/load.jl
@@ -20,11 +20,60 @@ Live load
 in general: 1.92 kN/m2 should do the work.
 Beware for 4.79 cases, since it's 2.5 times more than 1.92
 
-
+Dead load 
+    assume a 400x400 mm concrete section 
+    Concrete density = 2400 kg/m3
+    Dl = 0.4*0.4*2.4 = 0.384 kN/m - > 0.4 kN/m
 ")
 
-w = 1.2*4 + 1.6*(1.92*6)
+w = 1.2*0.4 + 1.6*(2.4*L)
 
 L  = 6
 
 w*L^2/8
+
+#span goes from 2 meters to 12 meters 
+#bay goes from 2 metrers to 8 meters
+using Makie, GLMakie, kjlMakie
+set_theme!(kjl_dark)
+spans = Vector{Float64}()
+bays = Vector{Float64}()
+Mdemands_office= Vector{Float64}()
+Mdemands_residential = Vector{Float64}()    
+for span = 2.0:0.5:12.0
+    for bay = 2:0.5:8
+        w_office = 1.2*0.4 + 1.6*(4.80*bay)
+        w_residential = 1.2*0.4 + 1.6*(1.92*bay)
+
+        Mdemand_office = w_office*span^2/8
+        Mdemand_residential = w_residential*span^2/8
+        push!(spans, span)
+        push!(bays, bay)
+        push!(Mdemands_office, Mdemand_office)
+        push!(Mdemands_residential, Mdemand_residential)
+    end
+end
+
+f1 = Figure(resolution = (1000,1000), title = "Name")
+ax1 = Axis(f1[1,1], title = "Beam spans vs Moment damands [Residential]",
+xlabel = "Spans [m]", ylabel = "Moment demands [kN.m]")
+scatter!(ax1, spans, Mdemands_residential, markersize = 10)
+ax2 = Axis(f1[1,2], title = "Bays vs Moment damands [Residential] ",
+xlabel = "Bays [m]", ylabel = "Moment demands [kN.m]")
+scatter!(ax2, bays, Mdemands_residential, markersize = 10)
+
+ax3 = Axis(f1[2,1], title = "Beam spans vs Moment damands [Office]",
+xlabel = "Spans [m]", ylabel = "Moment demands [kN.m]")
+scatter!(ax3, spans, Mdemands_office, markersize = 10, color = :green)
+ax4 = Axis(f1[2,2], title = "Bays vs Moment damands [Office] ",
+xlabel = "Bays [m]", ylabel = "Moment demands [kN.m]")
+scatter!(ax4, bays, Mdemands_office, markersize = 10, color = :green)
+
+
+
+
+f1
+#count points that are less than 500 kN.m
+count = 0
+
+# save("span_bay_load.png", f1)

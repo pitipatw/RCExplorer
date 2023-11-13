b41849f8949c85c51ff8d25841477dfb8973b6b3
Pitipat-PC <pitipatw@mit.edu>
2023-11-13 07:07:31 -0500

    create main() function
    Solve global/local variable issue.

--- src/PostTensioned/halfscaletest/input_data.jl
+++ src/PostTensioned/halfscaletest/input_data.jl
@@ -48,6 +48,7 @@ begin
     Mdec = ps_force*em
     concrete_force = ps_force*cos(24.0*pi/180.0)
     fpe = ps_force/Aps # Effective post tensioning stress [MPa] ***will input the one on the test day***
+    println("fpe/fpu = ", fpe/1860)
     ϵpe = fpe / Eps # Effective post tensioning strain [mm/mm]
     #find moment due to the applied force.
     ϵce = ps_force*em/Zb/Ec - concrete_force/Atr/Ec # effetive strain in the concrete [mm/mm]

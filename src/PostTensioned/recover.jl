803c7a281ec6ab60811e45abb4a7de9e27a1c6c7
pitipat_mac <pitipatw@mit.edu>
2023-11-16 23:13:32 -0500

    done design, but got straight line

--- src/PostTensioned/recover.jl
+++ src/PostTensioned/recover.jl
@@ -1,28 +1,32 @@
 using CSV, DataFrames, JSON
 using Dates
-include("Pt_Catalog.jl")
 
 """
 catalog format
 fc', as, ec, fpe, Pu, Mu, Vu, embodied
 """
-catalog = CSV.read(joinpath(@__DIR__,"Outputs\\output_static.csv"), DataFrame);
-sort!(catalog, [:carbon, :ec])
-#test input
-open(joinpath(@__DIR__,"JSON\\test_input.json"), "r") do f
+catalog = CSV.read(joinpath(@__DIR__,"Outputs/output_static.csv"), DataFrame);
+sort!(catalog, [:carbon, :fc′, :as, :ec])
+
+#load demands into a dictionary
+open(joinpath(@__DIR__,"json/test_input.json"), "r") do f
     global demands = DataFrame(JSON.parse(f, dicttype=Dict{String,Any}))
     ns = size(demands)[1]
     demands[!,:idx] = 1:ns
 end
 
+#python used 0 index, here, we shift those by 1.
 demands[!,"e_idx"] .+= 1
 demands[!, "s_idx"] .+=1
-e_idx = 1 
+
+#properly label the element index. Now there are repetitions.
+#Need to generate a new version that does not repeat.
+global e_idx = 1 
 for i in 1:size(demands)[1]
     if i !=1
         demands[i-1, :e_idx] = e_idx
         if demands[i,:s_idx] < demands[i-1,:s_idx]
-            e_idx +=1 
+            global e_idx +=1 
         end
         
     end
@@ -30,23 +34,28 @@ for i in 1:size(demands)[1]
         demands[i, :e_idx] = e_idx
     end
 end
-demands
+
+println(demands)
 # sort!(demands, [:e_idx, :s_idx])
 
 
 
 # function match_demands(demands, catalog)
-# sections_per_element = Dict( k => 0 for k in unique(demands[!, "e_idx"]))
+
+#map from element idx to section idx.
 elements_to_sections = Dict(k=> Int[] for k in unique(demands[!,"e_idx"]))
-#goes in a loop
+
+#count total number of sections and elements
 ns = size(demands)[1]
-ne = unique(demands[!,:e_idx]) #python starts at 0
+ne = unique(demands[!,:e_idx]) 
 
 #number of available choices
 nc = size(catalog,1)
+
+#map between demands and indx of the feasible section they have
 output_results = Dict{Int64, Vector{Int64}}()
 
-#go through each section.
+#go through each section and filter the feasible designs from the catalog.
 for i = 1:ns
     en = demands[i, "e_idx"]
     sn = demands[i, "s_idx"]
@@ -81,52 +90,81 @@ end
 # output_results = match_demands(demands,catalog)
 
 """
-Find the optimum result for each element. 
+Find the optimum result for each element.
+    
+    !! not optimum
 For the same element, will use the same fc′ steel size and post tensioning stress.
 
 """
 #function find_optimum(output_results, elements_to_sections, demands)
 ns = size(demands)[1]
 ne = unique(demands[!,:e_idx]) #python starts at 0
-element_designs = Dict(k=> DataFrame() for k in unique(demands[!,"e_idx"]))
+element_designs = Dict(k=> [[]] for k in unique(demands[!,"e_idx"]))
 #find fc′, as, and fpe that appear in all sections in an element.
 for i in ne #loop each element
+    println("working on element $i out of $(length(ne)) elements")
     sections = elements_to_sections[i]
+    #count number of sections 
+    local ns = length(sections)
+
+    #start from the middle-ish
+    mid = div(ns,2)
+
+    #get the feasible designs for the middle section
+    feasible_idx = output_results[sections[mid]]
     
+    sub_catalog = sort!(catalog[feasible_idx, :], [:carbon, :fc′, :as, :ec])
 
-    #reset choices
-    avai_fc′ = unique(catalog[!, :fc′])
-    avai_as = unique(catalog[!, :as])
-    avai_fpe = unique(catalog[!, :fpe])
+    #now, loop each design in the sub catalog, see if as and fpe are available in all sections.
+    #if not, remove that design from the sub catalog.
+    #if yes, keep it.
 
-    #sections index that are with that element.
-    # sections = filter(:e_idx => x-> x==i, demands)
-        filter!(e->e ∈ all_fc′, avai_fc′)
-        filter!(e->e ∈ all_as, avai_as)
-        filter!(e->e ∈ all_fpe, avai_fpe)
-        # println("#####")
-        # println(length(avai_fc′))
-        # println(length(avai_as))
-        # println(length(avai_fpe))
-        #now we filter the design space by avai...
-        #end
+    for d in eachrow(sub_catalog)
+        # all_fc′ = true
+        all_as = true
+        all_fpe = true
+        for s in sections
+            #check if the design is available in that section.
+            #if not, remove it from the sub catalog.
+            #if yes, keep it.
+            # if !(d[:fc′] ∈ catalog[output_results[s], :fc′])
+            #     all_fc′ = false
+            # end
+            if !(d[:as] ∈ catalog[output_results[s], :as])
+                all_as = false
+            end
+            if !(d[:fpe] ∈ catalog[output_results[s], :fpe])
+                all_fpe = false
+            end
+        end
+        if !(all_as && all_fpe)
+            println("before ",length(sub_catalog))
+            filter!(x->x!=d, sub_catalog)
+            println("after ",length(sub_catalog))
+        end
     end
 
-    for s in sections
-        feasible_idx = output_results[s]
-        sub_catalog = catalog[feasible_idx, :]
-        element_designs[s] = filter(:fc′ => x -> x ∈ avai_fc′, sub_catalog)
-        element_designs[s] = filter(:as  => x -> x ∈ avai_as , element_designs[s])
-        element_designs[s] = filter(:fpe => x -> x ∈ avai_fpe, element_designs[s])
-    end
+    sort!(sub_catalog, [:carbon, :fpe, :as, :fc′])
 
-element_designs
+    section_designs = Vector{Vector}(undef, ns)
+    for is in 1:ns
+        #select the first one because it's the lowest already.
+        select_ID= sub_catalog[1,:ID]
+        #find lowest e for this one.
+        section_designs[is] = collect(catalog[select_ID,:])
+        println(section_designs)
+        end
+element_designs[i] = section_designs
 
-final_designs = Vector{Vector}(undef, length(element_designs))
-for i in 1:length(element_designs) 
-    # available_designs = sort(element_designs[i],[:]
-    final_designs[i] = Array{Float64,1}(element_designs[i][1,:])
 end
+
+# final_designs = Vector{Vector}(undef, length(element_designs))
+# for i in 1:length(element_designs) 
+#     #just select the first one to design.
+#     final_designs[i] = Array{Float64,1}(element_designs[i][1,:])
+# end
+
+
 # final_designs2 = Vector{Vector}(undef, length(element_designs))
 # for i in 1:length(element_designs)
 #     sections = elements_to_sections[i]
@@ -140,33 +178,3 @@ end
 #     for s in sections 
         
 
-#final design vizualization 
-using Makie, GLMakie
-begin
-L = 200
-f1 = Figure(resolution = (1800,5000))
-Axes = Vector{Axis}(undef, length(ne))
-n = 3
-m = 4
-start = 1
-for i in eachindex(ne)
-    e = ne[i]
-    #find how many elements in that section.
-    sections = elements_to_sections[e]
-    @show ns = length(sections)
-    ix = div(i-1,n)+1
-    iy = mod(i-1,n)
-    # @show (ix,iy)
-    Axes[e] = Axis(f1[ix,iy], aspect = DataAspect(), title = "$e", xticks = 0:100:(ns*50),
-    #limits = (0,(ns+1)*50,-L,10)
-    )
-    
-    lines!(Axes[e], 50 .* (1:ns), -L.*(getindex.(final_designs[start:start+ns-1],3)))
-    text!( (10,-125), text = join(getindex.(final_designs[start:start+ns-1],1), " - "))
-
-    text!((10,-150), text = join(getindex.(final_designs[start:start+ns-1],2)," - ") )
-    text!((10,-175), text = join(getindex.(final_designs[start:start+ns-1],4)," - "))
-    start += ns
-end
-end
-f1
